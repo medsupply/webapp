ed7a5ee7d82e4b5e425d4fb2df8d37ebd5d7d934
Alejandro Sanchez <sanchezalejandro86@gmail.com>
2018-06-18 17:41:07 -0300

    Road map & FAQs

--- src/app/ico-landing/faqs/faqs.component.ts
+++ src/app/ico-landing/faqs/faqs.component.ts
@@ -9,7 +9,20 @@ export class FaqsComponent implements OnInit {
 
   constructor() { }
 
+  private active:number = -1;
+
   ngOnInit() {
   }
 
+  isActive(index:number){
+    return index == this.active;
+  }
+
+  toggle(index:number){
+    if(this.isActive(index)){
+      this.active = -1; // close everyone
+    }else{
+      this.active = index; // toogle only this one
+    }
+  }
 }
